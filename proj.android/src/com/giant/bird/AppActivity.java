0deec977a256503dc6e7001154b256f041f9cf10
AIRIA <chaibingcheng0305@gmail.com>
2014-09-15 15:02:23 +0800

    change package name

--- proj.android/src/com/giant/bird/AppActivity.java
+++ proj.android/src/com/giant/bird/AppActivity.java
@@ -24,9 +24,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-package org.cocos2dx.cpp;
+package com.giant.bird;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 
 public class AppActivity extends Cocos2dxActivity {
+	
 }
